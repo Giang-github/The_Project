350cf397a7b313e1b58f30d72123833cec81c96e
giang <gn6724130@gmail.com>
2021-01-25 16:18:41 +0700

    Update_index

--- index.php
+++ index.php
@@ -38,7 +38,7 @@
 	  </div>
 <nav class="navbar navbar-expand-lg  navbar-light bg-light" >
   <div  class="container" >
-	  <a class="nav-link" href="index.php"> Trang chủ </a>
+	  <a class="nav-link" href="index.php"> Home </a>
 	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 	    <span class="navbar-toggler-icon"></span>
 	  </button>
